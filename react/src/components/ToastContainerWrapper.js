c456525d0a6a1c1ba593c342edaca62be40e8659
IbragimKhaniev <newinterny@gmail.com>
2025-07-19 23:23:06 +0300

    Update files

--- react/src/components/ToastContainerWrapper.js
+++ react/src/components/ToastContainerWrapper.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Theme, useTheme } from '@mui/material/styles';
 
 /**
  * Wrapper component for ToastContainer to centralize toast notifications
+ * Styled to match the Facebook-inspired Material-UI theme
  */
 const ToastContainerWrapper = () => {
+  const theme: Theme = useTheme();
+
   return (
     <ToastContainer
       position="top-right"
@@ -17,6 +21,17 @@ const ToastContainerWrapper = () => {
       pauseOnFocusLoss
       draggable
       pauseOnHover
+      theme="colored"
+      toastStyle={{
+        backgroundColor: theme.palette.background.paper,
+        color: theme.palette.text.primary,
+        borderRadius: theme.shape.borderRadius,
+        boxShadow: theme.shadows[1],
+        fontFamily: theme.typography.fontFamily,
+      }}
+      progressStyle={{
+        backgroundColor: theme.palette.primary.main,
+      }}
     />
   );
 };
