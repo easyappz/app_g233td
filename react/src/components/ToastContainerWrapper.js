4c7ae5f8b68e338844a890b0d70a258710c7d1c9
IbragimKhaniev <newinterny@gmail.com>
2025-07-19 23:23:25 +0300

    Update files

--- react/src/components/ToastContainerWrapper.js
+++ react/src/components/ToastContainerWrapper.js
@@ -15,7 +15,7 @@ const ToastContainerWrapper = () => {
       position="top-right"
       autoClose={5000}
       hideProgressBar={false}
-      newestOnTop={false}
+      newestOnTop={true}
       closeOnClick
       rtl={false}
       pauseOnFocusLoss
@@ -26,8 +26,9 @@ const ToastContainerWrapper = () => {
         backgroundColor: theme.palette.background.paper,
         color: theme.palette.text.primary,
         borderRadius: theme.shape.borderRadius,
-        boxShadow: theme.shadows[1],
+        boxShadow: theme.shadows[2],
         fontFamily: theme.typography.fontFamily,
+        border: `1px solid ${theme.palette.divider}`,
       }}
       progressStyle={{
         backgroundColor: theme.palette.primary.main,
