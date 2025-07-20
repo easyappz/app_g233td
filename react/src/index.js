914cb8e40651609eb517932214dac4f7943681db
IbragimKhaniev <newinterny@gmail.com>
2025-07-20 10:45:17 +0300

    Update files

--- react/src/index.js
+++ react/src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import QueryProvider from './providers/QueryProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryProvider>
   </React.StrictMode>
 );
 
